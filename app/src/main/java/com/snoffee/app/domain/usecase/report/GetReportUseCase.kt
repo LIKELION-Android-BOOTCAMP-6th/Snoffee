f2af3d5291a40b48cc81bca26a6135f0ec6da947
JungSub <ljs990326@gmail.com>
2026-05-13 14:11:51 +0900

    리포트 Rename

--- app/src/main/java/com/snoffee/app/domain/usecase/report/GetReportUseCase.kt
+++ app/src/main/java/com/snoffee/app/domain/usecase/report/GetReportUseCase.kt
@@ -9,7 +9,7 @@ import javax.inject.Inject
 // 주간 카페인-수면 상관관계 리포트 조회 UseCase
 // ReportViewModel에서 호출
 // CaffeineRepository, SleepRepository를 통해 주간 데이터 조회
-class GetWeeklyReportUseCase @Inject constructor(
+class GetReportUseCase @Inject constructor(
     private val caffeineRepository: CaffeineRepository,  // Hilt가 자동 주입
     private val sleepRepository: SleepRepository          // Hilt가 자동 주입
 ) {
