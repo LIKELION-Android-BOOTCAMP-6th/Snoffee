e04aacf70dd2fc333e49a124947c110d187491b5
juu124 <juu12424@gmail.com>
2026-05-14 11:13:55 +0900

    [수정] 탭바 클릭시 앱 죽는 이슈 수정

--- app/src/main/java/com/snoffee/app/core/navigation/AppNavHost.kt
+++ app/src/main/java/com/snoffee/app/core/navigation/AppNavHost.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.snoffee.app.presentation.caffeine.CaffeineMainScreen
 
 // 각 스크린
 import com.snoffee.app.presentation.home.HomeScreen
@@ -30,27 +31,27 @@ fun AppNavHost(
         }
 
         //카페인 입력/목록 화면
-//        composable(Screen.Caffeine.route) {
-//            CaffeineMainScreen() // presentation.caffeine.CaffeineMainScreen
-//        }
-//        //수면 화면
-//        composable(Screen.Report.route) {
+        composable(Screen.Caffeine.route) {
+            CaffeineMainScreen() // presentation.caffeine.CaffeineMainScreen
+        }
+        //수면 화면
+        composable(Screen.Report.route) {
 //            SleepScreen() // presentation.report.ReportScreen
-//        }
-//
-//        //리포트 화면
-//        composable(Screen.Report.route) {
+        }
+
+        //리포트 화면
+        composable(Screen.Report.route) {
 //            ReportScreen() // presentation.report.ReportScreen
-//        }
-//
-//        //설정 화면
-//        composable(Screen.MySetting.route) {
+        }
+
+        //설정 화면
+        composable(Screen.MySetting.route) {
 //            MySettingScreen() // presentation.mySetting.MySettingScreen
-//        }
-//
-//        //온보딩
-//        composable(Screen.Onboarding.route) {
+        }
+
+        //온보딩
+        composable(Screen.Onboarding.route) {
 //            OnboardingScreen()
-//        }
+        }
     }
 }
