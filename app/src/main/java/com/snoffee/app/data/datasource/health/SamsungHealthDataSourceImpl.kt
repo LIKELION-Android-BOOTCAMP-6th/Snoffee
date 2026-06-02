68d8285f97308b2bde231ab9a1463051f2eb77bb
JungSub <ljs990326@gmail.com>
2026-06-02 09:11:43 +0900

    삼성 헬스 데이터 이슈 해결

--- app/src/main/java/com/snoffee/app/data/datasource/health/SamsungHealthDataSourceImpl.kt
+++ app/src/main/java/com/snoffee/app/data/datasource/health/SamsungHealthDataSourceImpl.kt
@@ -68,22 +68,22 @@ class SamsungHealthDataSourceImpl @Inject constructor(
         startTimeMillis: Long,
         endTimeMillis: Long
     ): List<SleepDataDto> {
-
         if (!hasPermissions()) {
             return emptyList()
         }
 
-        val now = System.currentTimeMillis()
+        val bufferedStartTimeMillis =
+            startTimeMillis - 12L * 60L * 60L * 1000L
 
-        val threeMonthsAgo =
-            now - (90L * 24L * 60L * 60L * 1000L)
+        val bufferedEndTimeMillis =
+            endTimeMillis + 12L * 60L * 60L * 1000L
 
         val response = healthConnectClient.readRecords(
             ReadRecordsRequest(
                 recordType = SleepSessionRecord::class,
                 timeRangeFilter = TimeRangeFilter.between(
-                    Instant.ofEpochMilli(threeMonthsAgo),
-                    Instant.ofEpochMilli(now)
+                    Instant.ofEpochMilli(bufferedStartTimeMillis),
+                    Instant.ofEpochMilli(bufferedEndTimeMillis)
                 )
             )
         )
