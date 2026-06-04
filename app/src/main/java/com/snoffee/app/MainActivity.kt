6da7227306f0d75ab4099705f21fceebb59b0d6a
juu124 <juu12424@gmail.com>
2026-06-04 11:40:27 +0900

    [추가] #59 앱 전체 폰트 스케일 하한, 상한 값 추가

--- app/src/main/java/com/snoffee/app/MainActivity.kt
+++ app/src/main/java/com/snoffee/app/MainActivity.kt
@@ -5,9 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.snoffee.app.core.navigation.AppNavHost
@@ -27,72 +30,81 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SnoffeeTheme {
-                val navController = rememberNavController()
+            // text 크기 최소 값, 최대 값 설정 (text 사이즈에 의해 ui가 깨지지 않게 위해서)
+            val density = LocalDensity.current
+            val cappedDensity = Density(
+                density = density.density,
+                fontScale = density.fontScale.coerceIn(0.95f, 1.2f)   // 하한 0.95, 상한 1.2
+            )
 
-                val isOnboardingCompleted by onboardingPreferenceDataSource
-                    .isOnboardingCompleted
-                    .collectAsState(initial = null)
+            CompositionLocalProvider(LocalDensity provides cappedDensity) {
+                SnoffeeTheme {
+                    val navController = rememberNavController()
 
-                if (isOnboardingCompleted == null) {
-                    return@SnoffeeTheme
-                }
+                    val isOnboardingCompleted by onboardingPreferenceDataSource
+                        .isOnboardingCompleted
+                        .collectAsState(initial = null)
 
-                val startDestination =
-                    if (isOnboardingCompleted == true) {
-                        Screen.Home.route
-                    } else {
-                        Screen.Onboarding.route
+                    if (isOnboardingCompleted == null) {
+                        return@SnoffeeTheme
                     }
 
-                //현재 경로 확인 (온보딩 등 특정 화면에서 앱바/탭바를 숨기기 위함)
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentRoute = navBackStackEntry?.destination?.route
+                    val startDestination =
+                        if (isOnboardingCompleted == true) {
+                            Screen.Home.route
+                        } else {
+                            Screen.Onboarding.route
+                        }
 
-                val topBarTitle = when (currentRoute) {
-                    Screen.Home.route -> "Snoffee"
-                    Screen.Caffeine.route -> "카페인 리포트"
-                    Screen.Sleep.route -> "수면 리포트"
-                    Screen.Report.route -> "리포트"
-                    Screen.Setting.route -> "설정"
-                    else -> "Snoffee"
-                }
+                    //현재 경로 확인 (온보딩 등 특정 화면에서 앱바/탭바를 숨기기 위함)
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val currentRoute = navBackStackEntry?.destination?.route
 
-                // 앱바·탭바를 숨길 화면 목록
-                val fullScreenRoutes = setOf(
-                    Screen.Onboarding.route,
-                    Screen.CaffeineSearch.route,
-                )
+                    val topBarTitle = when (currentRoute) {
+                        Screen.Home.route -> "Snoffee"
+                        Screen.Caffeine.route -> "카페인 리포트"
+                        Screen.Sleep.route -> "수면 리포트"
+                        Screen.Report.route -> "리포트"
+                        Screen.Setting.route -> "설정"
+                        else -> "Snoffee"
+                    }
 
-                //앱 전체 레이아웃
-                val isFullScreenRoute = currentRoute in fullScreenRoutes
-
-                if (isFullScreenRoute) {
-                    AppNavHost(
-                        navController = navController,
-                        modifier = Modifier,
-                        startDestination = startDestination
+                    // 앱바·탭바를 숨길 화면 목록
+                    val fullScreenRoutes = setOf(
+                        Screen.Onboarding.route,
+                        Screen.CaffeineSearch.route,
                     )
-                } else {
-                    Scaffold(
-                        topBar = {
-                            SnoffeeAppBar(
-                                title = topBarTitle,
-                                onNotificationClick = {
-                                    // 알림 아이콘 클릭 시 로직
-                                }
-                            )
-                        },
-                        bottomBar = {
-                            SnoffeeBottomBar(navController = navController)
-                        },
-                        containerColor = SnoffeeBgBase
-                    ) { innerPadding ->
+
+                    //앱 전체 레이아웃
+                    val isFullScreenRoute = currentRoute in fullScreenRoutes
+
+                    if (isFullScreenRoute) {
                         AppNavHost(
                             navController = navController,
-                            modifier = Modifier.padding(innerPadding),
+                            modifier = Modifier,
                             startDestination = startDestination
                         )
+                    } else {
+                        Scaffold(
+                            topBar = {
+                                SnoffeeAppBar(
+                                    title = topBarTitle,
+                                    onNotificationClick = {
+                                        // 알림 아이콘 클릭 시 로직
+                                    }
+                                )
+                            },
+                            bottomBar = {
+                                SnoffeeBottomBar(navController = navController)
+                            },
+                            containerColor = SnoffeeBgBase
+                        ) { innerPadding ->
+                            AppNavHost(
+                                navController = navController,
+                                modifier = Modifier.padding(innerPadding),
+                                startDestination = startDestination
+                            )
+                        }
                     }
                 }
             }
