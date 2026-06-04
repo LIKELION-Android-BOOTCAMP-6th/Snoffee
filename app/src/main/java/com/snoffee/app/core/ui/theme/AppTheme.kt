9cd525c6e47d0013853fa2fafd18cdc32ea9ff08
juu124 <juu12424@gmail.com>
2026-06-04 17:21:13 +0900

    [수정] #59 시스템 다크모드일 때 모든 화면이 다크 모드가 적용되지 않게 방지

--- app/src/main/java/com/snoffee/app/core/ui/theme/AppTheme.kt
+++ app/src/main/java/com/snoffee/app/core/ui/theme/AppTheme.kt
@@ -1,6 +1,5 @@
 package com.snoffee.app.core.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -126,7 +125,7 @@ val LocalSnoffeeColorScheme = staticCompositionLocalOf { SnoffeeLightColorScheme
 
 @Composable
 fun SnoffeeTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     content: @Composable () -> Unit,
 ) {
     val colorScheme = if (darkTheme) SnoffeeDarkColorScheme else SnoffeeLightColorScheme
